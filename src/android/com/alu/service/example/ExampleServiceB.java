7fcd4a7909e93bef7c3f0d7765db9415c10e75e2
Clément Perreau <clemguip@gmail.com>
2015-01-20 10:53:17 +0100

    Added second service

---
+++ src/android/com/alu/service/example/ExampleServiceB.java
@@ -0,0 +1,14 @@
+package com.alu.service.example;
+
+/**
+ * A second service, which is exactly the same as A to demonstrate how to register and run
+ * multiple services
+ * @author Clement Perreau
+ */
+public class ExampleServiceB extends ExampleServiceA {
+
+	protected String name = "B";
+	
+	protected int ID = 34158;
+	
+}
